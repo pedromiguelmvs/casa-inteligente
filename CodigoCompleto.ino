fc220873a14a0324bbf682a8edb1c85a1ebc9a6d
pedromiguelmvs <pedromiguelmvs@gmail.com>
2021-02-23 19:13:08 -0300

    modificações no arquivo completo

--- CodigoCompleto.ino
+++ CodigoCompleto.ino
@@ -16,20 +16,40 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
-const char fingerprint[] PROGMEM =  "5B FB D1 D4 49 D3 0F A9 C6 40 03 34 BA E0 24 05 AA D2 E2 01";
-char json[400] = {0};
- 
-int frequencia = 0; // Inicializa variável de frequência em 0
-int Buzzer = null; // Inicializa buzzer no pino 13 - procurar a pinagem do buzzer que não sabemos
-int duration, distance; // Inicializa variável de distância e tempo
+const char fingerprint[] PROGMEM = "5B FB D1 D4 49 D3 0F A9 C6 40 03 34 BA E0 24 05 AA D2 E2 01";
 
 int LED = D3;
-int LDR = A0; // Pino no qual o LDR está conectado
-int entrada = 0; // Variável que receberá o valor lido do LDR
+int LDR = A0; // LRD tá conectado aqui
+int input = 0; // recebe o valor lido de LDR
+ 
+int frequency = 0; // inicia frequência em 0
+int Buzzer = null; // pino do buzzer = ?? - não sabemos a pinagem do buzzer
+int duration, distance; // inicia variáveis de distância e tempo
 
-int LEDPis = D4;
+int teste_buzzer = D4;
 
+char json[400] = {0};
 StaticJsonDocument<256> doc;
+
+void setup() {
+  // usando a serial pra testes
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(teste_buzzer, OUTPUT);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(Buzzer, OUTPUT);
+  
+  // iniciando conexão wifi
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(SSID, PASSWD);
+
+  // acesse arduinojson.org/v6/assistant e passe uma amostra pra calcular a capacidade
+  const size_t capacity = JSON_OBJECT_SIZE(1) + JSON_ARRAY_SIZE(8) + 146;
+  DynamicJsonDocument doc(capacity);
+
+}
 
 void resultOfGet(String msg){
     memset(json,0,sizeof(json));
@@ -49,92 +69,70 @@ void resultOfGet(String msg){
     Serial.println(ticker_last);
 }
 
-void setup() {
-  //1 - Para testar, vamos usar a serial
-  Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-  pinMode(LEDPis, OUTPUT);
-
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(Buzzer, OUTPUT);
-  
-  //2 - iniciamos a conexão WiFi...
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(SSID, PASSWD);
-
-  //3 - acesse arduinojson.org/v6/assistant e passe uma amostra pra calcular a capacidade
-  const size_t capacity = JSON_OBJECT_SIZE(1) + JSON_ARRAY_SIZE(8) + 146;
-  DynamicJsonDocument doc(capacity);
-
-}
-
 void loop() {
-  Serial.println("loop started...");
-
-
+  Serial.println("loop iniciado");
        digitalWrite(trigPin, HIGH);
        delayMicroseconds(10);
        digitalWrite(trigPin, LOW);
        duration = pulseIn(echoPin, HIGH);
-       distance = (duration / 2) / 29.1; // Define base do cálculo de conversão
+       distance = (duration / 2) / 29.1; // cálculo de conversão
        
-       if (distance >= 40 || distance <= 0) { // Define as distâncias bases de verificação
-       Serial.println("Ninguem na area");
-       digitalWrite(LEDPis, LOW); // teste já q n tem pino de buzzer
+       if (distance >= 40 || distance <= 0) { // verificando distâncias
+        Serial.println("Ninguém por perto!");
+        digitalWrite(teste_buzzer, LOW); // teste (não sabe-se o pino do buzzer)
        }
        
        else {
-       Serial.println("Pessoa Detectada");
-       Serial.print("Distância= ");
-       Serial.print(distance); // Imprime o valor da distância no Monitor Serial
+        Serial.println("Tem alguém vindo!");
+        Serial.print("Distância = ");
+        Serial.print(distance); // imprime distância no serial
 
-       digitalWrite(Buzzer, HIGH); // acionando buzzer - caso queira testar o led, colocar o led da piscina no lugar do buzzer
-       
-       for (frequencia = 150; frequencia < 1800; frequencia += 1) { // Tone que produz sirene de polícia
-       tone(Buzzer, frequencia, tempo);
-       delay(3);
-       }
-       for (frequencia = 1800; frequencia > 150; frequencia -= 1) { // Tone que produz sirene de polícia
-       tone(Buzzer, frequencia, tempo);
-       delay(3);
-       }
-       }
+        digitalWrite(Buzzer, HIGH); // acionando buzzer - caso queira testar o led, colocar o led da piscina no lugar do buzzer
         
-        
-        entrada = analogRead(LDR);
-        Serial.println(entrada); // Valor que será exibido no Serial Monitor
-        if (entrada > 900) // Quanto maior a leitura, menor é a luminosidade
-        {
-        digitalWrite(LED, HIGH); // Acender o LED
+        for (frequency = 150; frequency < 1800; frequency += 1) { // Tone que produz sirene de polícia
+          tone(Buzzer, frequency, tempo);
+          delay(3);
         }
-        else
-        {
+        for (frequency = 1800; frequency > 150; frequency -= 1) { // Tone que produz sirene de polícia
+          tone(Buzzer, frequency, tempo);
+          delay(3);
+        }
+      }
+        
+        
+      input = analogRead(LDR);
+      Serial.println(input);
+      if (input > 900) // quanto maior a leitura, menor é a luminosidade
+      {
+        digitalWrite(LED, HIGH); // Acender o LED
+      }
+      else
+      {
         digitalWrite(LED, LOW); // Apagar o LED
         delay(100);
-        }
-        
-        if ((WiFiMulti.run() == WL_CONNECTED)){
-          std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
-          client->setFingerprint(fingerprint);
-          Serial.println("connected...");
-          //WiFiClient client;
+      }
+      
+      if ((WiFiMulti.run() == WL_CONNECTED)){
+        std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+        client->setFingerprint(fingerprint);
+        Serial.println("connected...");
+        //WiFiClient client;
 
-          HTTPClient http;
+        HTTPClient http;
 
-        //3 - iniciamos a URL alvo, pega a resposta e finaliza a conexão
-        if (http.begin(*client,"https://tein-g3.000webhostapp.com")){
+        // iniciamos a URL alvo, pega a resposta e finaliza a conexão
+        if (http.begin(*client,"https://fadasprojetotein.000webhostapp.com/")){
           Serial.println("http.begin ok");
         }
         int httpCode = http.GET();
-        if (httpCode > 0) { //Maior que 0, tem resposta a ser lida
+        if (httpCode > 0) { // caso seja maior que 0, tem resposta a ser lida
             String payload = http.getString();
 
             if (payload == "1") {
-              digitalWrite(LEDPis, HIGH);
+              digitalWrite(teste_buzzer, HIGH);
               
             } else {
-              digitalWrite(LEDPis, LOW);
+              digitalWrite(teste_buzzer, LOW);
               
             }
             
@@ -144,9 +142,9 @@ void loop() {
         }
         else {
           Serial.println(httpCode);
-            Serial.println("Falha na requisição");
+          Serial.println("Falha durante a requisição");
         }
-        http.end();
-        }
-        delay(5000);
+      http.end();
+      }
+  delay(5000);
 }
